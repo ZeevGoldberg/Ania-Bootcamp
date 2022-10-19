a607cde1230ce435fd4bd8778f51aea5cbc06395
Zeev Y. Goldberg <zeevygoldberg@gmail.com>
2022-10-19 20:42:50 +0300

    a great day

---
+++ JSTemplate/src/index.js
@@ -0,0 +1,22 @@
+let name = 'Zeev'
+
+function printName() {
+    console.log(name)
+}
+
+printName()
+
+let count = 0
+function increment(){
+    return count =+ 1
+}
+
+console.log(increment());
+
+const grandName = 'Zee'
+
+function sendMemo(){
+    console.log('Hey ' + grandName + ' You left your Eggs Boiling!');
+}
+
+sendMemo()
