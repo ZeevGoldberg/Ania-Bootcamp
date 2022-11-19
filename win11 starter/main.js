0e58b25ae2e453f8d8f6fdb6031c65e1d226b152
Zeev Y Goldberg <zeevygoldberg@gmail.com>
2022-11-20 00:00:18 +0200

    thank you hashem

---
+++ win11 starter/main.js
@@ -0,0 +1,9 @@
+const word = 'banana'
+
+console.log(word.length)
+
+const balls = document.querySelectorAll('.ball')
+
+const fruit = ['fig', 'date', 'pear', 'cherry', 'apricot', 'banana']
+console.log(fruit.length);
+
